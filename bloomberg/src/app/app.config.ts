be6d4c8bc6d664e7c1d0319d44056fdc2a2694bb
Ciaran <ciaranmccarthy100@gmail.com>
2026-04-23 21:39:40 +0100

    feat: enable progressive web app support with service worker and manifest configuration

--- bloomberg/src/app/app.config.ts
+++ bloomberg/src/app/app.config.ts
@@ -1,17 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideIonicAngular(),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(IonicStorageModule.forRoot())
-  ],
+    importProvidersFrom(IonicStorageModule.forRoot()),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
